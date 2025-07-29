c0dbc6304c761da996bb433e52b7f228b0328201
parkhyomi <phyomi3@naver.com>
2025-07-30 02:51:31 +0900

    [Design/#1 - 로그인 화면 버튼 빼고 완료]

--- app/src/main/java/com/example/shinhan_qna_aos/login/LoginScreen.kt
+++ app/src/main/java/com/example/shinhan_qna_aos/login/LoginScreen.kt
@@ -13,10 +13,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shinhan_qna_aos.R
+import com.example.shinhan_qna_aos.ui.theme.pretendard
 
 @Composable
 fun LoginScreen() {
@@ -25,63 +28,54 @@ fun LoginScreen() {
             .fillMaxSize()
             .systemBarsPadding() // 상태바+내비게이션 영역 침범 방지
             .background(Color.White)
+            .padding(top = 64.dp, start = 40.dp, end = 40.dp, bottom = 48.dp),
+        contentAlignment = Alignment.Center
     ) {
         val screenWidth = maxWidth
         val isCompact = screenWidth < 360.dp
 
-        // 조건에 따른 텍스트 스타일 적용
-        val titleStyle = if (isCompact) 32.sp else 25.sp
-        val descStyle = if (isCompact) 25.sp else 20.sp
-        val imageSize = if (isCompact) 60.dp else 100.dp
+
 
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = if (isCompact) 16.dp else 32.dp),
+            modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Image(painter = painterResource(R.drawable.biglogo), contentDescription = null, modifier = Modifier.size(imageSize))
-            Spacer(modifier = Modifier.height(20.dp))
-            // 카카오 로그인 버튼 (화면 너비에 따라 크기 조절)
-            Button(
-                onClick = {
-                /* TODO */
-                    // 로그인 인증 관련 코드 필요
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(if (isCompact) 48.dp else 56.dp),
-                shape = RoundedCornerShape(8.dp)
-            ) {
-                Text(
-                    text = "카카오 로그인",
-                    fontSize = descStyle
-                )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            // 구글 로그인 버튼 (화면 너비에 따라 크기 조절)
-            Button(
-                onClick = {
-                    /* TODO */
-                    // 로그인 인증 관련 코드 필요
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(if (isCompact) 48.dp else 56.dp),
-                shape = RoundedCornerShape(8.dp)
-            ) {
-                Text(
-                    text = "구글 로그인",
-                    fontSize = descStyle
-                )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(
-                text = "관리자 전용 페이지",
-                color = Color.Gray,
-                modifier = Modifier.clickable{ /**관리자 로그인**/ }
+            Image(
+                painter = painterResource(R.drawable.biglogo),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
             )
+
+            /**관리자 로그인**/
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){// 카카오 로그인 버튼 (화면 너비에 따라 크기 조절)
+                Image(
+                    painter = painterResource(R.drawable.kakao_login),
+                    contentDescription = "카카오 로그인",
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                // 구글 로그인 버튼 (화면 너비에 따라 크기 조절)
+                Image(
+                    painter = painterResource(R.drawable.google_login),
+                    contentDescription = "구글 로그인",
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                Text(
+                    text = "관리자 전용 페이지",
+                    color = Color(0xffDFDFDF),
+                    style = TextStyle(
+                        fontFamily = pretendard,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp
+                    ),
+                    modifier = Modifier.clickable { /**관리자 로그인**/ }
+                )
+            }
         }
     }
 }
