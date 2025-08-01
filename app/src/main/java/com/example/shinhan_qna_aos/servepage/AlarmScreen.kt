1ea219506bdf6fe8f3d94f81a3b8c39e7b5df47d
parkhyomi <phyomi3@naver.com>
2025-08-01 17:32:09 +0900

    [Design/#1 - 알림 화면]

--- app/src/main/java/com/example/shinhan_qna_aos/servepage/AlarmScreen.kt
+++ app/src/main/java/com/example/shinhan_qna_aos/servepage/AlarmScreen.kt
@@ -11,30 +11,37 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shinhan_qna_aos.R
+import com.example.shinhan_qna_aos.TopBar
 import com.example.shinhan_qna_aos.ui.theme.pretendard
 import com.jihan.lucide_icons.lucide
 
 @Composable
 fun AlarmScreen (){
-
+    Column {
+        TopBar("알림",{})
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(bottom = 50.dp)) {
+            items(6) { data ->
+                AlarmButton()
+                Divider()
+            }
+        }
+    }
 }
 
 @Composable
@@ -91,5 +98,6 @@ fun AlarmButton() {
 @Preview(showBackground = true)
 @Composable
 fun AlarmPreview(){
-    AlarmButton()
+    AlarmScreen()
+//    AlarmButton()
 }
