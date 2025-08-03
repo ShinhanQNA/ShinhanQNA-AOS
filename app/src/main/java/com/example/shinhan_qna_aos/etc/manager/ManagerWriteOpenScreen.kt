71191fd6ec1d7b34d6d2669e39e5fce42257f6d2
parkhyomi <phyomi3@naver.com>
2025-08-03 17:36:16 +0900

    [Design/#1 - 관리자용 게시판, 공지 열기]

---
+++ app/src/main/java/com/example/shinhan_qna_aos/etc/manager/ManagerWriteOpenScreen.kt
@@ -0,0 +1,170 @@
+package com.example.shinhan_qna_aos.etc.manager
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shinhan_qna_aos.DetailContent
+import com.example.shinhan_qna_aos.R
+import com.example.shinhan_qna_aos.TopBar
+import com.example.shinhan_qna_aos.ui.theme.pretendard
+import com.jihan.lucide_icons.lucide
+
+@Composable
+fun ManagerWriteOpenScreen(
+    isNotice: Boolean = true // true면 공지사항, false면 일반글
+) {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(bottom = 50.dp)
+        ) {
+            TopBar(null, {})
+            DetailContent()
+
+            if (!isNotice) {
+                Spacer(modifier = Modifier.height(16.dp))
+                LikeFlagBan(likeCount = 45, flagsCount = 10, banCount = 2)
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+            ManagerFunctionButton(isNotice)
+        }
+        // 배너광고는 항상 하단 고정
+        Text(
+            "배너광고",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Color.Red)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun LikeFlagBan(likeCount: Int, flagsCount: Int, banCount: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(24.dp)
+            .padding(horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        InfoIconCount(lucide.thumbs, "좋아요 표시", likeCount, Color.Black, 16)
+        InfoIconCount(R.drawable.flag, "신고 표시", flagsCount, Color(0xffFF9F43), 14)
+        InfoIconCount(lucide.ban, "차단 표시", banCount, Color(0xffFC4F4F), 14)
+    }
+}
+
+@Composable
+fun InfoIconCount(
+    icon: Int, // draw resource or vector
+    desc: String,
+    count: Int,
+    color: Color,
+    fontSize: Int
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = desc,
+            modifier = Modifier.size(16.dp),
+            tint = color
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            count.toString(),
+            color = color,
+            style = TextStyle(
+                fontFamily = pretendard,
+                fontWeight = FontWeight.Normal,
+                fontSize = fontSize.sp
+            ),
+        )
+    }
+}
+
+@Composable
+fun ManagerFunctionButton(isNotice: Boolean) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.End
+    ) {
+        ManagerButton(
+            icon = if (isNotice) lucide.trash else lucide.list,
+            label = if (isNotice) "삭제" else "검토",
+            background = Color(0xffFC4F4F)
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        ManagerButton(
+            icon = if (isNotice) R.drawable.square_pen else lucide.flag,
+            label = if (isNotice) "수정" else "경고",
+            background = if (isNotice) Color.Black else Color(0xffFF9F43)
+        )
+    }
+}
+
+@Composable
+fun ManagerButton(icon: Int, label: String, background: Color) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = Modifier
+            .background(background, RoundedCornerShape(12.dp))
+            .padding(horizontal = 12.dp, vertical = 8.dp)
+    ) {
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = label,
+            modifier = Modifier.size(20.dp),
+            tint = Color.White
+        )
+        Text(
+            text = label,
+            color = Color.White,
+            style = TextStyle(
+                fontFamily = pretendard,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WriteOpenScreenPreview(){
+    ManagerWriteOpenScreen()
+//    FunctionButton()
+}
